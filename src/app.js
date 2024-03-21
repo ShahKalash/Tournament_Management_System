b21df7eec4da32745f582c4539e5e29f709e1b2b
Kalash <kalash.s@ahduni.edu.in>
2024-03-21 13:27:22 +0530

    node js set

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+app.listen(port, () => {
+    console.log('server listening on port');
+})
