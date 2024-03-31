d77b34af64f1839812d0254374ee8d6b7a52996f
Aditya Agarwal <aditya.a1@ahduni.edu.in>
2024-03-31 11:10:48 +0530

    ^_^

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import queryString from "query-string";
 
 const app = express();
 const port = 3000;
@@ -9,7 +10,7 @@ const db = new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "TOURNAMENT_MANAGEMENT",
-    password: "Kalash@2911",
+    password: "12345",
     port: 5432,
 });
 
@@ -26,78 +27,139 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
 
+    console.log(req.query);
+    let bflag = false;
+    if(req.query)
+    {
+        bflag = req.query.dup_flag;
+    }
+
+    console.log("true or not");
+    console.log(bflag);
+    console.log("end");
+
+    try
+    {
     const temp = await db.query(
-        "SELECT * FROM USER_TYPE1;"
+        "SELECT * FROM USER_TYPE;"
     );
 
     let result = temp.rows;
-
-    console.log(result);
-
-    res.render('index.ejs', { 'result': result });
+    // console.log(result);
+    // , 'bflag': bool_flag
+    res.render('index.ejs', { 'result': result, 'bflag': bflag});
+    }
+    catch(e)
+    {
+        console.log(e);
+    }
 });
 
-// app.post("/add", async (req, res) => {
-//     let input = req.body["user_type"];
-//     input = input.toUpperCase();
-//     // console.log(input);
-//     try {
-//         await db.query(
-//             "INSERT INTO USER_TYPE (ut_desc) VALUES($1)",
-//             [input]
-//         );
-//         res.redirect("/");
-//     } catch (err) {
-//         console.log(err);
-//     }
+// app.get("/:first", async (req, res) => {
 
-// });
+//     let bool_flag = req.params.dup_flag;
+//     console.log(req.params);
 
-// app.post("/edit", async (req, res) => {
-//     let u_id = req.body.user_id;
+//     try
+//     {
+//     const temp = await db.query(
+//         "SELECT * FROM USER_TYPE;"
+//     );
 
-//     console.log(u_id);
-
-//     let temp = await db.query("SELECT * FROM USER_TYPE WHERE UT_ID = $1", [u_id]);
 //     let result = temp.rows;
-
-//     res.render("update.ejs", { 'result': result });
-
-// });
-
-// app.post("/update", async (req, res) => {
-//     const id = req.body.user_id;
-//     const u_type = req.body.user_type;
-
-//     console.log(id);
-//     console.log(u_type);
-//     try {
-//         await db.query(
-//             "UPDATE USER_TYPE SET UT_DESC = ($1) WHERE UT_ID = $2",
-//             [u_type, id]
-//         );
-//         res.redirect("/");
-//     } catch (err) {
-//         console.log(err);
+//     // console.log(result);
+    
+//     res.render('index.ejs', { 'result': result, 'bflag': bool_flag});
 //     }
-
-// });
-
-// app.post("/delete", async (req, res) => {
-//     const id = req.body.user_id;
-
-//     console.log(id);
-//     try {
-//         await db.query(
-//             "DELETE FROM USER_TYPE WHERE UT_ID = $1",
-//             [id]
-//         );
-//         res.redirect("/");
-//     } catch (err) {
-//         console.log(err);
+//     catch(e)
+//     {
+//         console.log(e);
 //     }
-
 // });
+
+app.post("/add", async (req, res) => {
+    let input = req.body["user_type"];
+    input = input.toUpperCase();
+
+    let temp = await db.query(
+        "SELECT * FROM USER_TYPE WHERE UPPER(UT_DESC) LIKE '%"+ input + "%';"
+    );
+
+    console.log("a");
+    let result = temp.rows;
+    console.log("b");
+    console.log(result.length);
+    console.log("a");
+
+    if(result.length > 0)
+    {
+        const query = queryString.stringify({
+            "dup_flag": true
+        });
+        res.redirect('/?' + query);
+    }
+    else
+    {
+        try {
+            await db.query(
+                "INSERT INTO USER_TYPE (ut_desc) VALUES($1)",
+                [input]
+            );
+            res.redirect("/");
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    // console.log(input);
+
+
+});
+
+app.post("/edit", async (req, res) => {
+    let u_id = req.body.user_id;
+
+    // console.log(u_id);
+
+    let temp = await db.query("SELECT * FROM USER_TYPE WHERE UT_ID = $1", [u_id]);
+    let result = temp.rows;
+
+    res.render("update.ejs", { 'result': result });
+
+});
+
+app.post("/update", async (req, res) => {
+    const id = req.body.user_id;
+    const u_type = req.body.user_type;
+
+    // console.log(id);
+    // console.log(u_type);
+    try {
+        await db.query(
+            "UPDATE USER_TYPE SET UT_DESC = ($1) WHERE UT_ID = $2",
+            [u_type, id]
+        );
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+app.post("/delete", async (req, res) => {
+    const id = req.body.user_id;
+
+    // console.log(id);
+    try {
+        await db.query(
+            "DELETE FROM USER_TYPE WHERE UT_ID = $1",
+            [id]
+        );
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+    }
+
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
